703a653422da614c579d72d92111739771557404
Лихачев Арсений Алексеевич <likhachev-ara@local.dart.prm.ru>
2020-07-09 11:40:05 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,133 @@
+const express = require('express')
+const fs = require('fs')
+const fse = require('fs-extra')
+const cors = require('cors')
+const path = require('path')
+const remove = require('remove');
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+let disksList = []
+let fileTypes = [
+    { fileType: 'video' , fileExt: '.wmv'},
+    { fileType: 'video' , fileExt: '.mp4'},
+    { fileType: 'video' , fileExt: '.avi'},
+    { fileType: 'audio' , fileExt: '.mp3'},
+    { fileType: 'picture' , fileExt: '.png'},
+    { fileType: 'picture' , fileExt: '.jpeg'},
+    { fileType: 'document' , fileExt: '.txt'},
+    { fileType: 'document' , fileExt: '.doc'},
+    { fileType: 'document' , fileExt: '.docx'},
+    { fileType: 'document' , fileExt: '.xls'},
+    { fileType: 'document' , fileExt: '.xlsx'},
+    { fileType: 'program' , fileExt: '.exe'}
+]
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.text())
+
+app.use(cors())
+
+app.get('/', (req, res) => res.sendFile('index.html'))
+
+app.post('/', (req,res) =>{
+    res.json(directoryContent(path.normalize(req.body)))
+})
+
+app.post('/move', (req,res) =>{
+    fse.move(JSON.parse(req.body).obj, JSON.parse(req.body).pathTo, err => {
+        if (err) return console.error(err)
+        res.json({
+            dir1: directoryContent(path.normalize(path.parse(JSON.parse(req.body).obj).dir)),
+            dir2: directoryContent(path.normalize(path.parse(JSON.parse(req.body).pathTo).dir))
+        })
+    })
+})
+
+app.post('/copy', (req,res) =>{
+    fse.copy(JSON.parse(req.body).obj, JSON.parse(req.body).pathTo, err => {
+        if (err) return console.error(err)
+        res.json({
+            dir1: directoryContent(path.normalize(path.parse(JSON.parse(req.body).obj).dir)),
+            dir2: directoryContent(path.normalize(path.parse(JSON.parse(req.body).pathTo).dir))
+        })
+    })
+})
+
+app.post('/remove', (req,res) =>{
+    try {
+        remove.removeSync(path.normalize(req.body))
+    } catch(err) {
+        console.log(err)
+    }
+    res.json(directoryContent(path.parse(path.normalize(req.body)).dir))
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+function directoryContent(dirPath){
+    let fileList = []
+    fs.readdirSync(dirPath).forEach((arg) => {
+        try {
+            let stat = fs.statSync(path.join(dirPath,arg))
+            fileList.push({
+                fileName: arg,
+                type: (stat.isDirectory()) ? 'directory' : typeOfFile(arg),
+                size: (stat.isDirectory()) ? ' ' : readableSize(stat.size),
+                changeTime: stat.mtimeMs
+            })
+        }
+        catch(err) {
+            console.log(err)
+        }
+    })
+
+    fileList.sort((a ,b) => {
+        if(a.type == 'directory')
+            if(b.type == 'directory')
+                return 0
+            else 
+                return -1
+        else 
+            if(b.type == 'directory')
+                return 1
+            else 
+                return 0
+    })
+
+    return {
+        path: dirPath,
+        fileList: fileList,
+        disksList: disksList
+    }
+}
+
+function typeOfFile(name) {
+    return (fileTypes.find( file => file.fileExt === path.extname(name))) ? fileTypes.find( file => file.fileExt === path.extname(name)).fileType : 'file'
+}
+
+function readableSize(size) {
+    var i = 0, type = ['б','Кб','Мб','Гб','Тб','Пб'];
+    while((size / 1000 | 0) && i < type.length - 1) {
+	    size /= 1024;
+	    i++;
+    }
+    
+	return size.toFixed(2) + ' ' + type[i];
+}
+
+let ch = 'a'
+
+while (ch<='z') {
+    try {
+        fs.accessSync(ch + ':/')
+        disksList.push(ch.toUpperCase() + ':/')
+    }
+    catch(err) {
+    }
+	ch = String.fromCharCode(ch.charCodeAt(0)+1);		
+}
---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="footer">
+        <button v-on:click = "$emit('move')" class='button'>Переместить</button>
+        <button v-on:click = "$emit('copy')" class='button'>Скопировать</button>
+        <button v-on:click = "$emit('delete')" class='button'>Удалить</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'    
+}
+</script>
+
+<style>
+.footer {
+    display: flex;
+    height: 15vh;
+    width: 100vw;
+}
+.button {
+    width: 15vw;
+    height: 5vh;
+    margin: auto;
+    border-radius: 5px;
+    background-color:white;
+    cursor: pointer;
+}
+</style>
